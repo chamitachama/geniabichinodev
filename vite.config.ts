ab6e1eea0ab7f29f084d2a63c910aa89c2da8e6b
Genesis Iabichino <geniabichino@gmail.com>
2025-10-10 16:29:11 +0200

    deploy config for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-    tsconfigPaths(),
-  
-  ],
-  alias: {
-    "@": path.resolve(__dirname, "./src"),
-    "@components": path.resolve(__dirname, "./src/components"),
-    "@pages": path.resolve(__dirname, "./src/pages"),
-  },
-})
+  plugins: [
+    react(),
+    tsconfigPaths()
+  ]
+});
